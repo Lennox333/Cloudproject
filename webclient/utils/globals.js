512aa82fc46bbc4f3912f1c714803056bcefd583
Lennox333 <anhnguyen607090@gmail.com>
2025-08-31 01:44:01 +1000

    sdf

--- webclient/utils/globals.js
+++ webclient/utils/globals.js
@@ -1,18 +1,5 @@
-let SERVER = "http://localhost:5000"; // fallback
-
-// Fetch the actual server URL from config.json
-let serverReady = fetch("/config.json")
-  .then(res => res.json())
-  .then(cfg => {
-    SERVER = cfg.SERVER;
-    console.log("Server URL set to:", SERVER);
-  });
-
-// Dynamically fetch without having to rebuild ecr images when ec2 instance is down and then dns changed
-const fetchServer = async (path, options = {}) => {
-  await serverReady; // ensures SERVER is loaded
-  return fetch(`${SERVER}${path}`, options);
-};
+const SERVER = "http://ec2-3-107-19-197.ap-southeast-2.compute.amazonaws.com:5000"; 
 
 
-export {serverReady, SERVER}
+
+export {SERVER}
