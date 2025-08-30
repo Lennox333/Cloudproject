f6ce00ecf6ef2f4594c5cf062c266b09a688f624
Lennox333 <anhnguyen607090@gmail.com>
2025-08-31 05:52:41 +1000

    sdf

--- server/utils/ffmpeg.js
+++ server/utils/ffmpeg.js
@@ -26,7 +26,7 @@ async function transcodeVideo(videoPath, videoId) {
   await fs.mkdir(uploadsDir, { recursive: true });
 
   const resolutions = [
-    // { name: `${videoId}_360p.mp4`, scale: "640:360" },
+    { name: `${videoId}_360p.mp4`, scale: "640:360" },
     { name: `${videoId}_480p.mp4`, scale: "854:480" },
     { name: `${videoId}_720p.mp4`, scale: "1280:720" },
   ];
