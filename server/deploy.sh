a66fbed66236e423dd2de63c759d84f956c80cb2
Lennox333 <anhnguyen607090@gmail.com>
2025-09-23 13:39:33 +1000

    deploy bash

---
+++ server/deploy.sh
@@ -0,0 +1,8 @@
+docker-compose down
+docker stop $(docker ps -aq)
+docker rm $(docker ps -aq)
+docker rmi -f $(docker images -q)
+docker volume rm $(docker volume ls -q)
+
+git pull
+docker-compose up -d
