0e562caf5f2393cf4902a526ea8ae0277d90cb6b
Lennox333 <anhnguyen607090@gmail.com>
2025-10-26 11:43:41 +1000

    asd

--- server/utils/envManager.js
+++ server/utils/envManager.js
@@ -29,9 +29,9 @@ const PARAMETERS = {
 // };
 
 // Secrets Manager name from env
-const SECRETS_MANAGER_NAME = process.env.COGNITO_SECRET_NAME;
+// const SECRETS_MANAGER_NAME = process.env.COGNITO_SECRET_NAME;
 
-// const SECRETS_MANAGER_NAME = "n11772891-cognito-secrets";
+const SECRETS_MANAGER_NAME = "n11772891-cognito-secrets";
 // AWS SDK Clients
 const ssmClient = new SSMClient({ region: AWS_REGION });
 const secretsManagerClient = new SecretsManagerClient({ region: AWS_REGION });
